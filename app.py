421789601747a59952621c5a0f5f011a125fdefe
Denes <denes.kosztyuk@gmail.com>
2023-11-10 23:16:46 +0000

    removed contact html, css, added mailto to main

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
     return render_template('main.html')
-
-@app.route('/contact', methods=['GET', 'POST'])
-def contact():
-    if request.method == 'POST':
-        name = request.form.get('name')
-        email = request.form.get('email')
-        message = request.form.get('message')
-        # You would process the form data here (e.g., send an email)
-        # After processing the form, redirect to a new page or render the page with a thank-you message.
-        return redirect(url_for('main'))  # Redirect to the main page as an example
-    return render_template('contact.html')
 
 @app.route('/projects')
 def projects():
